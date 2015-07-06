8cd5e1cd6f077d902c84588083f68c9760c69ba8
Frederiksen,Steven J <sfrederiksen@mitre.org>
2015-07-06 16:45:43 -0400

    Nightly build

--- D3Testin/scripts/app.js
+++ D3Testin/scripts/app.js
@@ -2,10 +2,54 @@
  * Created by sfrederiksen on 7/6/2015.
  */
 (function(){
-    angular.module("myModule",[])
+    angular.module("myApp",[])
 
 
         .controller("mainController",function($scope, $window){
+
+            $scope.display = function(key){
+                alert(key);
+            };
+            /*
+             Helper functions
+             */
+            $scope.windowWidth = function () {
+                return $window.innerWidth;
+            };
+            $scope.windowHeight = function () {
+                return $window.innerHeight;
+            };
+            $scope.findObjIndex = function (arry, key, value) {
+                for (var ix = 0; ix < arry.length; ix++) {
+                    if (arry[ix][key] === value) {
+                        return ix;
+                    }
+                }
+                return -1;
+            };
+            $scope.activeTab = 'hm';
+
+            $scope.setActive = function(key){
+                $scope.activeTab=key;
+            };
+
+            $scope.tabs = [
+                {
+                    name:"Home",
+                    key:"hm",
+                    active:true
+                },
+                {
+                    name:"Other",
+                    key:"ot",
+                    active:false
+                },
+                {
+                    name:"Moar",
+                    key:"mo",
+                    active:false
+                }
+            ];
 
 
             
